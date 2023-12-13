c01d5d0e2d5ee7addc3e4304a9467f5bec50de11
D E Costa <d.ethan.costa@gmail.com>
2023-12-13 17:58:11 -0500

    Rename cmd/timeruler to cmd/tr-serveR

---
+++ cmd/tr-server/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	//"io"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+const DefaultPort = 6576
+
+var Address = "127.0.0.1"
+
+func main() {
+
+	s := Server{
+		Owner: "",
+		Addr:  "",
+	}
+
+	// TODO ensure port value is valid
+	var port int
+	var standalone bool
+	flag.IntVar(&port, "p", DefaultPort, "The port that the server will run on")
+	flag.BoolVar(&standalone, "sa", false, "Whether or not the server is run locally (StandAlone)")
+	flag.Parse()
+	portStr := strconv.Itoa(port)
+
+	if standalone {
+		err := SetPid(Address, portStr)
+		if err != nil {
+			panic(err)
+		}
+		//log.SetOutput(io.Discard)
+	}
+
+	router := mux.NewRouter()
+	// TODO update API to use
+	// - GET /schedule instead of /get, POST /schedule instead of /build, PUT /schedule instead of /update
+	// - GET /current instead of /current, POST /current instead of /change_current
+	router.Handle("/get", http.HandlerFunc(s.GetSchedule))
+	router.Handle("/build", http.HandlerFunc(s.BuildSchedule))
+	router.Handle("/current", http.HandlerFunc(s.GetCurrentTask))
+	router.Handle("/change_current", http.HandlerFunc(s.ChangeCurrentTask))
+	router.Handle("/update", http.HandlerFunc(s.UpdateTasks))
+
+	log.Printf("Running on %s\n", portStr)
+	err := http.ListenAndServe(Address+":"+portStr, router)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Stopped running on %s\n", portStr)
+}
---
+++ cmd/tr-server/standalone.go
@@ -0,0 +1,77 @@
+package main
+
+// This includes the code used when running timeruler
+// as a standalone application on your local machine.
+// When this is the case, the serverUrl in the config
+// file will be `http://localhost:6756`
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"syscall"
+
+	gap "github.com/muesli/go-app-paths"
+)
+
+func SetPid(address, port string) error {
+	scope := gap.NewScope(gap.User, "timeruler")
+	configPath, err := scope.ConfigPath("config.json")
+	if err != nil {
+		return err
+	}
+	config := make(map[string]string)
+	configFile, err := os.OpenFile(configPath, os.O_RDWR, 0644)
+	if err != nil && os.IsNotExist(err) {
+		configFile, err = os.Create(configPath)
+		if err != nil {
+			return err
+		}
+		configFile.Close()
+	} else if err != nil {
+		return err
+	} else {
+		configBytes, err := io.ReadAll(configFile)
+		if err != nil {
+			configFile.Close()
+			return err
+		}
+		configFile.Close()
+
+		err = json.Unmarshal(configBytes, &config)
+		if err != nil {
+			return err
+		}
+	}
+
+	pid := syscall.Getpid()
+	if _, ok := config["pid"]; ok {
+		return errors.New(fmt.Sprintf("Config file shows server already running at pid %s", config["pid"]))
+	} else {
+		config["pid"] = strconv.Itoa(pid)
+	}
+
+	if _, ok := config["server"]; !ok {
+		config["server"] = "http://" + address + ":" + port
+	}
+
+	configBytes, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+
+	configFile, err = os.OpenFile(configPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		return err
+	}
+	defer configFile.Close()
+	_, err = configFile.Write(configBytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
